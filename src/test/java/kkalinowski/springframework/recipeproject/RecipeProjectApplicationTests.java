390c6ac89d0dfffdc2d9043ad7e8079c177e762a
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-17 12:39:43 +0100

    initial commit

---
+++ src/test/java/kkalinowski/springframework/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kkalinowski.springframework.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
