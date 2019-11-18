d1e402cd1dcccfbdab27a041116ec8be4e036535
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-18 17:46:37 +0100

    Recipe and Notes Entities

---
+++ src/main/java/kkalinowski/springframework/recipeproject/domain/Notes.java
@@ -0,0 +1,37 @@
+package kkalinowski.springframework.recipeproject.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by Krzysztof Kalinowski on 18/11/2019.
+ */
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
