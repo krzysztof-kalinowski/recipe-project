e0c50f94f700d8a85be3885097e6d2034d7a0e74
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-20 20:29:49 +0100

    First Unit Tests

---
+++ src/test/java/kkalinowski/springframework/recipeproject/domain/CategoryTest.java
@@ -0,0 +1,35 @@
+package kkalinowski.springframework.recipeproject.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long id = 4L;
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+        String desc = "description";
+        category.setDescription(desc);
+        assertEquals(desc, category.getDescription());
+    }
+
+    @Test
+    void getRecipes() {
+        
+    }
+}
