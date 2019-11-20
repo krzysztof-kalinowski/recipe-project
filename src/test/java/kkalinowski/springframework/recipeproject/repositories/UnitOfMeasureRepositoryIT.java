f1afd5d3433eaefc033971ce5366190c0be7b5cc
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-20 21:54:09 +0100

    Integration Test for UnitOfMeasureRepository

---
+++ src/test/java/kkalinowski/springframework/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,35 @@
+package kkalinowski.springframework.recipeproject.repositories;
+
+import kkalinowski.springframework.recipeproject.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+    }
+}
